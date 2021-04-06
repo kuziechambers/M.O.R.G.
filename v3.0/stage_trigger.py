7ae992af1ebbb1564ab04377a30021d30ac0c835
Kuzie-RasPi <kuzie.chambers@gmail.com>
2021-04-06 12:58:25 -0500

    logs update and state_trigger update

--- v3.0/stage_trigger.py
+++ v3.0/stage_trigger.py
@@ -16,33 +16,36 @@ from constants_time import *
 from text_service import send_text
 
 
+time.sleep(30.0)
 
-stage_time = "03-01-2021 12:00:00.000000"
+
+stage_time = "2021-03-01 12:00:00.000000"
 
 last_motion = dt.datetime.strptime(stage_time, '%Y-%m-%d %H:%M:%S.%f')
 
 while True:
-    # Get sensor states
-    motion = get_motion_state()
+	# Get sensor states
+	motion = get_motion_state()
 
-    # Get time values
-    now = dt.datetime.now()
-    now_date = now.date()
-    now_time = now.time()
-    last_motion_date = last_motion.date()
-    last_motion_time = last_motion.time()
-    seconds_away = (now - last_motion).total_seconds()
-    weekday = dt.datetime.now().weekday()
+	# Get time values
+	now = dt.datetime.now()
+	now_date = now.date()
+	now_time = now.time()
+	last_motion_date = last_motion.date()
+	last_motion_time = last_motion.time()
+	seconds_away = (now - last_motion).total_seconds()
+	weekday = dt.datetime.now().weekday()
 
-    if motion is True:
-        if seconds_away > 3600:
-            turnon_outlet()
-            play_sound(sounds['s_wake2'])
-            play_sound(sounds['s_brightlightseq'])
-            bright_lights_on()
-            play_sound(sounds['s_sequencecomplete'])
-            play_sound(sounds['s_up'])
-            turnoff_outlet()
+	if motion is True:
+		if seconds_away > 3600:
+			time.sleep(1.5)
+			turnon_outlet()
+			play_sound(sounds['s_wake2'])
+			play_sound(sounds['s_brightlightseq'])
+			bright_lights_on()
+			play_sound(sounds['s_sequencecomplete'])
+			play_sound(sounds['s_sevensummers'])
+			turnoff_outlet()
 
 
 
