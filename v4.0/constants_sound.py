5969cd487b252817cfb6401da410209b4fa3dca2
Kuzie-MacB <kuzie.chambers@gmail.com>
2021-04-06 21:11:05 -0500

    BIG CHANGES, MORG now has a new voice! lots of sound files have been updated

--- v4.0/constants_sound.py
+++ v4.0/constants_sound.py
@@ -3,55 +3,60 @@ import requests
 import simpleaudio as audio
 from audioplayer import AudioPlayer
 
-def sound_play(path):
-    AudioPlayer(path).play(block=True)
-
 
 # sound filenames dictionary
 sounds = {
-    's_anycompany': "/home/pi/M.O.R.G./sounds_wav/anycompany.wav",
-    's_anyplans': "/home/pi/M.O.R.G./sounds_wav/anyplans.wav",
-    's_allyourn': "/home/pi/M.O.R.G./sounds_wav/allyourn.wav",
-    's_brightlight': "/home/pi/M.O.R.G./sounds_wav/brightlight.wav",
-    's_brightlightseq': "/home/pi/M.O.R.G./sounds_wav/brightlightseq.wav",
-    's_chiquita': "/home/pi/M.O.R.G./sounds_wav/chiquita2.wav",
-    's_dimmedlight': "/home/pi/M.O.R.G./sounds_wav/dimmedlight.wav",
-    's_dimmedlightseq': "/home/pi/M.O.R.G./sounds_wav/dimmedlightseq.wav",
-    's_echobackinblack': "/home/pi/M.O.R.G./sounds_wav/echobackinblack.wav",
-    's_echoCFB': "/home/pi/M.O.R.G./sounds_wav/echoCFB.wav",
-    's_echohighwaytohell': "/home/pi/M.O.R.G./sounds_wav/echohighwaytohell.wav",
-    's_freebird': "/home/pi/M.O.R.G./sounds_wav/freebird2.wav",
-    's_goodafternoon': "/home/pi/M.O.R.G./sounds_wav/goodafternoon.wav",
-    's_goodevening': "/home/pi/M.O.R.G./sounds_wav/goodevening.wav",
-    's_goodmorning': "/home/pi/M.O.R.G./sounds_wav/goodmorning.wav",
-    's_onlygoodmorning': "/home/pi/M.O.R.G./sounds_wav/onlygoodmorning.wav",
-    's_heressomemusic': "/home/pi/M.O.R.G./sounds_wav/heressomemusic.wav",
-    's_holidaylilnasx': "/home/pi/M.O.R.G./sounds_wav/holidaylilnasx.wav",
-    's_howssomemusic': "/home/pi/M.O.R.G./sounds_wav/howssomemusic.wav",
-    's_howwasyourafternoon': "/home/pi/M.O.R.G./sounds_wav/howwasyourafternoon.wav",
-    's_productiveday1': "/home/pi/M.O.R.G./sounds_wav/productiveday1.wav",
-    's_productiveday2': "/home/pi/M.O.R.G./sounds_wav/productiveday2.wav",
-    's_ping': "/home/pi/M.O.R.G./sounds_wav/ping3.wav",
-    's_quietping': "/home/pi/M.O.R.G./sounds_wav/pingquiet2.wav",
-    's_saturdaybackinblack': "/home/pi/M.O.R.G./sounds_wav/saturdaybackinblack.wav",
-    's_saturdayhighwaytohell': "/home/pi/M.O.R.G./sounds_wav/saturdayhighwaytohell.wav",
-    's_sequencecomplete': "/home/pi/M.O.R.G./sounds_wav/sequencecomplete.wav",
-    's_sevensummers': "/home/pi/M.O.R.G./sounds_wav/sevensummers.wav",
-    's_up': "/home/pi/M.O.R.G./sounds_wav/up.wav",
-    's_whatspoppin': "/home/pi/M.O.R.G./sounds_wav/whatspoppin.wav",
-    's_withoutyou': "/home/pi/M.O.R.G./sounds_wav/withoutyou.wav",
-    's_welcomemrchambers1': "/home/pi/M.O.R.G./sounds_wav/welcomehomemrchambers1.wav",
-    's_welcomemrchambers2': "/home/pi/M.O.R.G./sounds_wav/welcomehomemrchambers2.wav",
-    's_welcomehomesir': "/home/pi/M.O.R.G./sounds_wav/welcomehomesir.wav",
-    's_watermelonsugar': "/home/pi/M.O.R.G./sounds_wav/watermelonsugar.wav",
-    's_wayout': "/home/pi/M.O.R.G./sounds_wav/wayout.wav",
-    's_wake': "/home/pi/M.O.R.G./sounds_wav/wake.wav",
-    's_wake2': "/home/pi/M.O.R.G./sounds_wav/wake3.wav"
+    's_anycompany': "/home/pi/M.O.R.G./sounds_IBM/anycompany.wav",
+    's_anyplans': "/home/pi/M.O.R.G./sounds_IBM/anyplans.wav",
+    's_allyourn': "/home/pi/M.O.R.G./sounds_IBM/AllYourn.wav",
+    's_brightlight': "/home/pi/M.O.R.G./sounds_IBM/brightlight.wav",
+    's_chiquita': "/home/pi/M.O.R.G./sounds_IBM/Chiquita.wav",
+    's_dimlight': "/home/pi/M.O.R.G./sounds_IBM/dimlight.wav",
+    's_freebird': "/home/pi/M.O.R.G./sounds_IBM/FreeBird.wav",
+    's_fridayheresmusic_m': "/home/pi/M.O.R.G./sounds_IBM/fridayheresmusic.wav",
+    's_fridayhowsmusic_m': "/home/pi/M.O.R.G./sounds_IBM/fridayhowsmusic.wav",
+    's_fridaymorning': "/home/pi/M.O.R.G./sounds_IBM/fridaymorning.wav",
+    's_goodafternoon_g': "/home/pi/M.O.R.G./sounds_IBM/goodafternoon_g.wav",
+    's_goodevening_g': "/home/pi/M.O.R.G./sounds_IBM/goodevening_g.wav",
+    's_goodmorning_g': "/home/pi/M.O.R.G./sounds_IBM/goodmorning_g.wav",
+    's_lightson': "/home/pi/M.O.R.G./sounds_IBM/lightson.wav",
+    's_mondaymorning': "/home/pi/M.O.R.G./sounds_IBM/mondaymorning.wav",
+    's_morninggreatday': "/home/pi/M.O.R.G./sounds_IBM/morninggreatday.wav",
+    's_morningproductive': "/home/pi/M.O.R.G./sounds_IBM/morningproductive.wav",
+    's_morninghadfun': "/home/pi/M.O.R.G./sounds_IBM/morninghadfun.wav",
+    's_morningproductiveday': "/home/pi/M.O.R.G./sounds_IBM/morningproductiveday.wav",
+    's_morningsleptwell': "/home/pi/M.O.R.G./sounds_IBM/morningsleptwell.wav",
+    's_heressomemusic_m': "/home/pi/M.O.R.G./sounds_IBM/heressomemusic.wav",
+    's_holiday': "/home/pi/M.O.R.G./sounds_IBM/Holiday.wav",
+    's_howssomemusic_m': "/home/pi/M.O.R.G./sounds_IBM/howssomemusic.wav",
+    's_howwasyourafternoon': "/home/pi/M.O.R.G./sounds_IBM/howwasyourafternoon.wav",
+    's_productiveday_m': "/home/pi/M.O.R.G./sounds_IBM/productiveday.wav",
+    's_ping': "/home/pi/M.O.R.G./sounds_IBM/ping3.wav",
+    's_quietping': "/home/pi/M.O.R.G./sounds_IBM/pingquiet2.wav",
+    's_saturdaybackinblack_m': "/home/pi/M.O.R.G./sounds_IBM/saturdaybackinblack.wav",
+    's_saturdayhighwaytohell_m': "/home/pi/M.O.R.G./sounds_IBM/saturdayhighwaytohell.wav",
+    's_saturdayheresmusic_m': "/home/pi/M.O.R.G./sounds_IBM/saturdayheresmusic.wav",
+    's_saturdayhowsmusic_m': "/home/pi/M.O.R.G./sounds_IBM/saturdayhowsmusic.wav",
+    's_saturdaysequence_m': "/home/pi/M.O.R.G./sounds_IBM/saturdaysequence.wav",
+    's_sevensummers': "/home/pi/M.O.R.G./sounds_IBM/7summers.wav",
+    's_tuesdaymorning': "/home/pi/M.O.R.G./sounds_IBM/tuesdaymorning.wav",
+    's_thursdaymorning': "/home/pi/M.O.R.G./sounds_IBM/thursdaymorning.wav",
+    's_toosieslide': "/home/pi/M.O.R.G./sounds_IBM/ToosieSlide.wav",
+    's_up': "/home/pi/M.O.R.G./sounds_IBM/Up.wav",
+    's_wednesdaymorning': "/home/pi/M.O.R.G./sounds_IBM/wednesdaymorning.wav",
+    's_whatspoppin': "/home/pi/M.O.R.G./sounds_IBM/WhatsPoppin.wav",
+    's_withoutyou': "/home/pi/M.O.R.G./sounds_IBM/WithoutYou.wav",
+    's_welcomemrchambers_g': "/home/pi/M.O.R.G./sounds_IBM/mrchambers_g.wav",
+    's_welcomehome_g': "/home/pi/M.O.R.G./sounds_IBM/welcomehome_g.wav",
+    's_welcomeback_g': "/home/pi/M.O.R.G./sounds_IBM/welcomeback_g.wav",
+    's_watermelonsugar': "/home/pi/M.O.R.G./sounds_IBM/WatermelonSugar.wav",
+    's_wayout': "/home/pi/M.O.R.G./sounds_IBM/Wayout.wav",
+    's_wake': "/home/pi/M.O.R.G./sounds_IBM/wake.wav"
 }
 
 # play sound function
-def play_sound(sound_file):
-    audio.WaveObject.from_wave_file(sound_file).play().wait_done()
+def play_sound(path):
+    AudioPlayer(path).play(block=True)
 
 
 # light payloads
--- v4.0/tts_service.py
+++ v4.0/tts_service.py
@@ -1,9 +1,12 @@
 from ibm_watson import TextToSpeechV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import json
-from constants_sound import sound_play
+from constants_sound import play_sound
 
-s_heressomemusic = "Welcome home sir, I presume you’re having a wonderful day. Here’s some music to keep it going."
+
+
+#text = [line.replace('\n','') for line in text]
+#text = ''.join(str(line) for line in text)
+
 
 ibm_api_key = 'xd5WDO7vtjDEf5v9jzmYbdbRG6zLm2AK4PBmk8y6PxCV'
 ibm_api_url = 'https://api.us-south.text-to-speech.watson.cloud.ibm.com/instances/ba47dae7-3648-4a3c-9df9-679f789c4fd2'
@@ -14,19 +17,31 @@ text_to_speech = TextToSpeechV1(
 )
 text_to_speech.set_service_url(ibm_api_url)
 
-def tts_play(sound_string, filename):
-    full_path = "/Users/kuziechambers/PycharmProjects/M.O.R.G./v4.0/" + filename
-    with open(full_path, 'wb') as audio_file:
+voices = text_to_speech.list_voices().get_result()
+#print(json.dumps(voices, indent=2))
+
+def tts_play(ssml_file, filename):
+    ssml_path = "/Users/kuziechambers/PycharmProjects/M.O.R.G./ssml_files/" + ssml_file
+    sound_path = "/Users/kuziechambers/PycharmProjects/M.O.R.G./ssml_files/" + filename + ".wav"
+
+    with open(ssml_path, 'r') as f:
+        text = f.read()
+
+    with open(sound_path, 'wb') as audio_file:
         audio_file.write(
             text_to_speech.synthesize(
-                sound_string,
-                voice='en-GB_JamesV3Voice',
+                text,
+                #voice='en-US_HenryV3Voice',
+                #voice='en-US_KevinV3Voice',
+                voice='en-GB_JamesV3Voice',# 'en-GB_JamesV3Voice' "en-US_MichaelVoice"
                 accept='audio/wav'
             ).get_result().content)
 
-    sound_play(full_path)
+    play_sound(sound_path)
 
-tts_play(s_heressomemusic, "heressomemusic")
+
+tts_play("x_saturdaysequence", "saturdaysequence")
+#tts_play("x_morningproductiveday", "morningproductiveday")
 
 
 
