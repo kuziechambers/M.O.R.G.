6d27516f062846fa3a96dee8bb376b69fef32966
Kuzie-MacB <kuzie.chambers@gmail.com>
2021-04-12 14:35:30 -0500

    fixing stage_trigger to actually exit

--- v4.5/stage_trigger.py
+++ v4.5/stage_trigger.py
@@ -25,41 +25,46 @@ stage_time = "2021-03-01 12:00:00.000000"
 last_motion = dt.datetime.strptime(stage_time, '%Y-%m-%d %H:%M:%S.%f')
 
 while True:
-    # Get sensor states
-    motion = get_motion_state()
+    try:
+        # Get sensor states
+        motion = get_motion_state()
 
-    # Get time values
-    now = dt.datetime.now()
-    now_date = now.date()
-    now_time = now.time()
-    last_motion_date = last_motion.date()
-    last_motion_time = last_motion.time()
-    seconds_away = (now - last_motion).total_seconds()
-    weekday = dt.datetime.now().weekday()
+        # Get time values
+        now = dt.datetime.now()
+        now_date = now.date()
+        now_time = now.time()
+        last_motion_date = last_motion.date()
+        last_motion_time = last_motion.time()
+        seconds_away = (now - last_motion).total_seconds()
+        weekday = dt.datetime.now().weekday()
 
-    if motion is True:
-        if seconds_away > 3600:
-            # turnon_outlet()
-            # time.sleep(1.0)
-            # play_sound(sounds['s_wake'])
-            # play_sound(sounds['s_welcomeback_g'])
-            # bright_lights_on()
-            # time.sleep(1.0)
-            # play_sound(sounds['s_eveningwelcomeback2'])
-            # #play_sound(sounds['s_up'])
-            # turnoff_outlet()
+        if motion is True:
+            if seconds_away > 3600:
+                # turnon_outlet()
+                # time.sleep(1.0)
+                # play_sound(sounds['s_wake'])
+                # play_sound(sounds['s_welcomeback_g'])
+                # bright_lights_on()
+                # time.sleep(1.0)
+                # play_sound(sounds['s_eveningwelcomeback2'])
+                # #play_sound(sounds['s_up'])
+                # turnoff_outlet()
 
-            play_sound('/home/pi/M.O.R.G./sounds_IBM/degrees/82.wav')
+                play_sound('/home/pi/M.O.R.G./sounds_IBM/degrees/82.wav')
 
-            turnon_outlet()
-            time.sleep(1.0)
-            play_sound(sounds['s_wake'])
-            play_sound(sounds['s_goodmorning_g'])
-            concentrate_lights_on()
-            weather_update()
-            time.sleep(1.0)
-            play_sound(sounds['s_mondaymorning'])
-            turnoff_outlet()
+                turnon_outlet()
+                time.sleep(1.0)
+                play_sound(sounds['s_wake'])
+                play_sound(sounds['s_goodmorning_g'])
+                concentrate_lights_on()
+                weather_update()
+                time.sleep(1.0)
+                play_sound(sounds['s_mondaymorning'])
+                turnoff_outlet()
+    except:
+        ex = sys.exc_info()
+        print(ex)
+        exit()
 
 
 
