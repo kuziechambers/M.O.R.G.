fd1056eecacd8b3a5c66d59e0a9c2f67baf96de5
Kuzie-MacB <kuzie.chambers@gmail.com>
2021-04-22 13:20:11 -0500

    trying to get the pyaudio stream to stop when silent

--- v5.0/test.py
+++ v5.0/test.py
@@ -8,30 +8,32 @@ from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 import concurrent.futures
 import pyaudio
 
-def valid_input_devices(py):
-    """
-    See which devices can be opened for microphone input.
-    call this when no PyAudio object is loaded.
-    """
-    mics = []
-    for device in range(py.get_device_count()):
-        if py.is_format_supported(device):
-            mics.append(device)
-    if len(mics) == 0:
-        print("no microphone devices found!")
-    else:
-        print("found %d microphone devices: %s" % (len(mics), mics))
-    return mics
+# def valid_input_devices(py):
+#     """
+#     See which devices can be opened for microphone input.
+#     call this when no PyAudio object is loaded.
+#     """
+#     mics = []
+#     for device in range(py.get_device_count()):
+#         if py.is_format_supported():
+#             mics.append(device)
+#     if len(mics) == 0:
+#         print("no microphone devices found!")
+#     else:
+#         print("found %d microphone devices: %s" % (len(mics), mics))
+#     return mics
+#
+#
+#
+#
+# print()
+# print()
+# print()
+# print()
+# valid_input_devices(p)
 
 
 p = pyaudio.PyAudio()
-
-print()
-print()
-print()
-print()
-valid_input_devices(p)
-
 
 print(p.get_default_input_device_info())
 print()
